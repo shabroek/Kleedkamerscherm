c183e7b73f26b624e9e0bbfca0199ba6c777b0e1
Sander van Broekhoven <sander1984@hotmail.com>
2019-01-19 08:45:12 +0100

    shrink wedstrijdmodel

--- src/app/models/wedstrijd.model.ts
+++ src/app/models/wedstrijd.model.ts
@@ -1,25 +1,16 @@
 export interface IWedstrijd {
     wedstrijddatum: Date;
-    teamnaam: string;
     thuisteam: string;
     uitteam: string;
-    teamvolgorde: number;
     competitiesoort: string;
-    competitie: string;
-    klasse: string;
-    poule: string;
-    klassepoule: string;
-    kaledatum: string;
     datum: string;
     aanvangstijd: string;
     wedstrijd: string;
     status: string;
-    scheidsrechters: string;
     scheidsrechter: string;
     veld: string;
     kleedkamerthuisteam: string;
     kleedkameruitteam: string;
     kleedkamerscheidsrechter: string;
-    meer: string;
 }
 
