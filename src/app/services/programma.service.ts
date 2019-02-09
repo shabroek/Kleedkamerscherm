b2994ee0fd6abfb6f0d0906e00937f0850f0bb98
Sander van Broekhoven <sander1984@hotmail.com>
2019-02-09 09:34:44 +0100

    sorteer op thuisteamnaam

--- src/app/services/programma.service.ts
+++ src/app/services/programma.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IWedstrijd } from '../models/wedstrijd.model';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,17 @@ export class ProgrammaService {
   constructor(private http: HttpClient) {
   }
   getProgramma(days: number): Observable<IWedstrijd[]> {
-    return this.http.get<IWedstrijd[]>(this.programmaUrl + '&aantaldagen=' + days);
+    return this.http.get<IWedstrijd[]>(this.programmaUrl + '&aantaldagen=' + days).pipe(
+      map(programma => programma.sort(this.sortByName)));
+  }
+
+  sortByName(a: IWedstrijd, b: IWedstrijd) {
+    if (a.thuisteam < b.thuisteam) {
+      return -1;
+    }
+    if (a.thuisteam > b.thuisteam) {
+      return 1;
+    }
+    return 0;
   }
 }
