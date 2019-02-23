0dc302f61037d5a56b33bd79b88ce7b53c3054ce
Sander van Broekhoven <sander1984@hotmail.com>
2019-02-23 10:17:15 +0100

    fix tests

--- src/app/services/programma.service.spec.ts
+++ src/app/services/programma.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProgrammaService } from './programma.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProgrammaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  }));
 
   it('should be created', () => {
     const service: ProgrammaService = TestBed.get(ProgrammaService);
