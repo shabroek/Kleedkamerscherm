6da029635f8d6174703df3783ca2916759d4619a
Sander van Broekhoven <s.vanbroekhoven@isah.com>
2025-09-27 15:13:43 +0200

    fixes

--- src/app/components/programma/wedstrijd-row.component.ts
+++ src/app/components/programma/wedstrijd-row.component.ts
@@ -1,14 +1,10 @@
 import { Component, Input } from "@angular/core";
 import { IWedstrijd } from "../../models/wedstrijd.model";
-import { KleedkamerPipe } from "../../pipes/kleedkamer.pipe";
-import { VeldTypePipe } from "../../pipes/veldtype.pipe";
-import { VeldPipe } from "../../pipes/veld.pipe";
 
 @Component({
   selector: "app-wedstrijd-row",
   templateUrl: "./wedstrijd-row.component.html",
   styleUrls: ["./wedstrijd-row.component.scss"],
-  imports: [KleedkamerPipe, VeldTypePipe, VeldPipe],
 })
 export class WedstrijdRowComponent {
   @Input() wedstrijd!: IWedstrijd;
